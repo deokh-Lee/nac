ee89d9ce0ebb1c8b02fa114721ffde08a0ebdfbe
deokh-Lee <hanuhm@naver.com>
2026-05-23 13:48:01 +0900

    Add EXTRACT_ELEC_DOC domain class

---
+++ src/main/java/com/saltlux/nac/elecdoc/ExtractElecDoc.java
@@ -0,0 +1,131 @@
+package com.saltlux.nac.elecdoc;
+
+public class ExtractElecDoc {
+
+    private String fileName;
+    private String rcRfileNo;
+    private String rcRitemNo;
+    private String contents;
+    private String imgDatas;
+    private String fileType;
+    private String fileGubun;
+    private String indexingContents;
+    private String hwpSubTitle;
+    private Integer dataYear;
+    private String queueState;
+    private String hasContents;
+    private String extractStatus;
+    private String extractErrMsg;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getRcRfileNo() {
+        return rcRfileNo;
+    }
+
+    public void setRcRfileNo(String rcRfileNo) {
+        this.rcRfileNo = rcRfileNo;
+    }
+
+    public String getRcRitemNo() {
+        return rcRitemNo;
+    }
+
+    public void setRcRitemNo(String rcRitemNo) {
+        this.rcRitemNo = rcRitemNo;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public String getImgDatas() {
+        return imgDatas;
+    }
+
+    public void setImgDatas(String imgDatas) {
+        this.imgDatas = imgDatas;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFileGubun() {
+        return fileGubun;
+    }
+
+    public void setFileGubun(String fileGubun) {
+        this.fileGubun = fileGubun;
+    }
+
+    public String getIndexingContents() {
+        return indexingContents;
+    }
+
+    public void setIndexingContents(String indexingContents) {
+        this.indexingContents = indexingContents;
+    }
+
+    public String getHwpSubTitle() {
+        return hwpSubTitle;
+    }
+
+    public void setHwpSubTitle(String hwpSubTitle) {
+        this.hwpSubTitle = hwpSubTitle;
+    }
+
+    public Integer getDataYear() {
+        return dataYear;
+    }
+
+    public void setDataYear(Integer dataYear) {
+        this.dataYear = dataYear;
+    }
+
+    public String getQueueState() {
+        return queueState;
+    }
+
+    public void setQueueState(String queueState) {
+        this.queueState = queueState;
+    }
+
+    public String getHasContents() {
+        return hasContents;
+    }
+
+    public void setHasContents(String hasContents) {
+        this.hasContents = hasContents;
+    }
+
+    public String getExtractStatus() {
+        return extractStatus;
+    }
+
+    public void setExtractStatus(String extractStatus) {
+        this.extractStatus = extractStatus;
+    }
+
+    public String getExtractErrMsg() {
+        return extractErrMsg;
+    }
+
+    public void setExtractErrMsg(String extractErrMsg) {
+        this.extractErrMsg = extractErrMsg;
+    }
+}
