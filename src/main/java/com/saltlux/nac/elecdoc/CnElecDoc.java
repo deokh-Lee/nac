ba0a8d5c3f00b5058022b58f7596118ef0a3363f
deokh-Lee <hanuhm@naver.com>
2026-05-23 13:47:38 +0900

    Add CN_ELEC_DOC domain class

---
+++ src/main/java/com/saltlux/nac/elecdoc/CnElecDoc.java
@@ -0,0 +1,169 @@
+package com.saltlux.nac.elecdoc;
+
+import java.time.LocalDate;
+
+public class CnElecDoc {
+
+    private String transferYear;
+    private String rcRfileNo;
+    private String rcRitemNo;
+    private String mngNo;
+    private Short detNo;
+    private Short detConnNo;
+    private String kikwanCode;
+    private String prodYear;
+    private String opnYn;
+    private String orgFilePath;
+    private String orgFileName;
+    private String saveFileName;
+    private String delYn;
+    private String chkYn;
+    private LocalDate fstRegDt;
+    private String fstRegId;
+    private LocalDate lstUpdDt;
+    private String lstUpdId;
+
+    public String getTransferYear() {
+        return transferYear;
+    }
+
+    public void setTransferYear(String transferYear) {
+        this.transferYear = transferYear;
+    }
+
+    public String getRcRfileNo() {
+        return rcRfileNo;
+    }
+
+    public void setRcRfileNo(String rcRfileNo) {
+        this.rcRfileNo = rcRfileNo;
+    }
+
+    public String getRcRitemNo() {
+        return rcRitemNo;
+    }
+
+    public void setRcRitemNo(String rcRitemNo) {
+        this.rcRitemNo = rcRitemNo;
+    }
+
+    public String getMngNo() {
+        return mngNo;
+    }
+
+    public void setMngNo(String mngNo) {
+        this.mngNo = mngNo;
+    }
+
+    public Short getDetNo() {
+        return detNo;
+    }
+
+    public void setDetNo(Short detNo) {
+        this.detNo = detNo;
+    }
+
+    public Short getDetConnNo() {
+        return detConnNo;
+    }
+
+    public void setDetConnNo(Short detConnNo) {
+        this.detConnNo = detConnNo;
+    }
+
+    public String getKikwanCode() {
+        return kikwanCode;
+    }
+
+    public void setKikwanCode(String kikwanCode) {
+        this.kikwanCode = kikwanCode;
+    }
+
+    public String getProdYear() {
+        return prodYear;
+    }
+
+    public void setProdYear(String prodYear) {
+        this.prodYear = prodYear;
+    }
+
+    public String getOpnYn() {
+        return opnYn;
+    }
+
+    public void setOpnYn(String opnYn) {
+        this.opnYn = opnYn;
+    }
+
+    public String getOrgFilePath() {
+        return orgFilePath;
+    }
+
+    public void setOrgFilePath(String orgFilePath) {
+        this.orgFilePath = orgFilePath;
+    }
+
+    public String getOrgFileName() {
+        return orgFileName;
+    }
+
+    public void setOrgFileName(String orgFileName) {
+        this.orgFileName = orgFileName;
+    }
+
+    public String getSaveFileName() {
+        return saveFileName;
+    }
+
+    public void setSaveFileName(String saveFileName) {
+        this.saveFileName = saveFileName;
+    }
+
+    public String getDelYn() {
+        return delYn;
+    }
+
+    public void setDelYn(String delYn) {
+        this.delYn = delYn;
+    }
+
+    public String getChkYn() {
+        return chkYn;
+    }
+
+    public void setChkYn(String chkYn) {
+        this.chkYn = chkYn;
+    }
+
+    public LocalDate getFstRegDt() {
+        return fstRegDt;
+    }
+
+    public void setFstRegDt(LocalDate fstRegDt) {
+        this.fstRegDt = fstRegDt;
+    }
+
+    public String getFstRegId() {
+        return fstRegId;
+    }
+
+    public void setFstRegId(String fstRegId) {
+        this.fstRegId = fstRegId;
+    }
+
+    public LocalDate getLstUpdDt() {
+        return lstUpdDt;
+    }
+
+    public void setLstUpdDt(LocalDate lstUpdDt) {
+        this.lstUpdDt = lstUpdDt;
+    }
+
+    public String getLstUpdId() {
+        return lstUpdId;
+    }
+
+    public void setLstUpdId(String lstUpdId) {
+        this.lstUpdId = lstUpdId;
+    }
+}
