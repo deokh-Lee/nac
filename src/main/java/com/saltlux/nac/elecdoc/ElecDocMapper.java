54649e09a45d1b707d0a3ec3386634120533db63
deokh-Lee <hanuhm@naver.com>
2026-05-23 13:49:59 +0900

    Add electronic document MyBatis mapper interface

---
+++ src/main/java/com/saltlux/nac/elecdoc/ElecDocMapper.java
@@ -0,0 +1,15 @@
+package com.saltlux.nac.elecdoc;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface ElecDocMapper {
+
+    List<CnElecDoc> findTargetDocuments(@Param("transferYear") String transferYear,
+                                         @Param("limit") int limit,
+                                         @Param("offset") int offset);
+
+    int upsertExtractDocument(ExtractElecDoc extractElecDoc);
+}
