773e453a522f7e1d9c38b77971ddfb2b42a43542
deokh-Lee <hanuhm@naver.com>
2026-05-23 12:59:50 +0900

    Add application context test

---
+++ src/test/java/com/saltlux/nac/NacApplicationTests.java
@@ -0,0 +1,12 @@
+package com.saltlux.nac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
