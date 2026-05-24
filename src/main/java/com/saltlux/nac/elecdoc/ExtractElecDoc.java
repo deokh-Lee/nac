88b2c5b59a9c57ea4233d4a95f60d7531f867c47
deokh-Lee <hanuhm@naver.com>
2026-05-24 19:07:11 +0900

    Add metadata columns to extract entity

--- src/main/java/com/saltlux/nac/elecdoc/ExtractElecDoc.java
+++ src/main/java/com/saltlux/nac/elecdoc/ExtractElecDoc.java
@@ -15,126 +15,49 @@ public class ExtractElecDoc {
     private Integer dataYear;
     private String queueState;
     private String hasContents;
+    private String implementOrg;
+    private String implementDate;
+    private String receiptOrg;
+    private String receiptDate;
     private String extractStatus;
     private String extractErrMsg;
 
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getRcRfileNo() {
-        return rcRfileNo;
-    }
-
-    public void setRcRfileNo(String rcRfileNo) {
-        this.rcRfileNo = rcRfileNo;
-    }
-
-    public String getRcRitemNo() {
-        return rcRitemNo;
-    }
-
-    public void setRcRitemNo(String rcRitemNo) {
-        this.rcRitemNo = rcRitemNo;
-    }
-
-    public Integer getZipSeq() {
-        return zipSeq;
-    }
-
-    public void setZipSeq(Integer zipSeq) {
-        this.zipSeq = zipSeq;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
-    public String getImgDatas() {
-        return imgDatas;
-    }
-
-    public void setImgDatas(String imgDatas) {
-        this.imgDatas = imgDatas;
-    }
-
-    public String getFileType() {
-        return fileType;
-    }
-
-    public void setFileType(String fileType) {
-        this.fileType = fileType;
-    }
-
-    public String getFileGubun() {
-        return fileGubun;
-    }
-
-    public void setFileGubun(String fileGubun) {
-        this.fileGubun = fileGubun;
-    }
-
-    public String getIndexingContents() {
-        return indexingContents;
-    }
-
-    public void setIndexingContents(String indexingContents) {
-        this.indexingContents = indexingContents;
-    }
-
-    public String getHwpSubTitle() {
-        return hwpSubTitle;
-    }
-
-    public void setHwpSubTitle(String hwpSubTitle) {
-        this.hwpSubTitle = hwpSubTitle;
-    }
-
-    public Integer getDataYear() {
-        return dataYear;
-    }
-
-    public void setDataYear(Integer dataYear) {
-        this.dataYear = dataYear;
-    }
-
-    public String getQueueState() {
-        return queueState;
-    }
-
-    public void setQueueState(String queueState) {
-        this.queueState = queueState;
-    }
-
-    public String getHasContents() {
-        return hasContents;
-    }
-
-    public void setHasContents(String hasContents) {
-        this.hasContents = hasContents;
-    }
-
-    public String getExtractStatus() {
-        return extractStatus;
-    }
-
-    public void setExtractStatus(String extractStatus) {
-        this.extractStatus = extractStatus;
-    }
-
-    public String getExtractErrMsg() {
-        return extractErrMsg;
-    }
-
-    public void setExtractErrMsg(String extractErrMsg) {
-        this.extractErrMsg = extractErrMsg;
-    }
+    public String getFileName() { return fileName; }
+    public void setFileName(String fileName) { this.fileName = fileName; }
+    public String getRcRfileNo() { return rcRfileNo; }
+    public void setRcRfileNo(String rcRfileNo) { this.rcRfileNo = rcRfileNo; }
+    public String getRcRitemNo() { return rcRitemNo; }
+    public void setRcRitemNo(String rcRitemNo) { this.rcRitemNo = rcRitemNo; }
+    public Integer getZipSeq() { return zipSeq; }
+    public void setZipSeq(Integer zipSeq) { this.zipSeq = zipSeq; }
+    public String getContents() { return contents; }
+    public void setContents(String contents) { this.contents = contents; }
+    public String getImgDatas() { return imgDatas; }
+    public void setImgDatas(String imgDatas) { this.imgDatas = imgDatas; }
+    public String getFileType() { return fileType; }
+    public void setFileType(String fileType) { this.fileType = fileType; }
+    public String getFileGubun() { return fileGubun; }
+    public void setFileGubun(String fileGubun) { this.fileGubun = fileGubun; }
+    public String getIndexingContents() { return indexingContents; }
+    public void setIndexingContents(String indexingContents) { this.indexingContents = indexingContents; }
+    public String getHwpSubTitle() { return hwpSubTitle; }
+    public void setHwpSubTitle(String hwpSubTitle) { this.hwpSubTitle = hwpSubTitle; }
+    public Integer getDataYear() { return dataYear; }
+    public void setDataYear(Integer dataYear) { this.dataYear = dataYear; }
+    public String getQueueState() { return queueState; }
+    public void setQueueState(String queueState) { this.queueState = queueState; }
+    public String getHasContents() { return hasContents; }
+    public void setHasContents(String hasContents) { this.hasContents = hasContents; }
+    public String getImplementOrg() { return implementOrg; }
+    public void setImplementOrg(String implementOrg) { this.implementOrg = implementOrg; }
+    public String getImplementDate() { return implementDate; }
+    public void setImplementDate(String implementDate) { this.implementDate = implementDate; }
+    public String getReceiptOrg() { return receiptOrg; }
+    public void setReceiptOrg(String receiptOrg) { this.receiptOrg = receiptOrg; }
+    public String getReceiptDate() { return receiptDate; }
+    public void setReceiptDate(String receiptDate) { this.receiptDate = receiptDate; }
+    public String getExtractStatus() { return extractStatus; }
+    public void setExtractStatus(String extractStatus) { this.extractStatus = extractStatus; }
+    public String getExtractErrMsg() { return extractErrMsg; }
+    public void setExtractErrMsg(String extractErrMsg) { this.extractErrMsg = extractErrMsg; }
 }
